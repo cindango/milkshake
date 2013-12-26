124c235f46c8cfbda0ac8c92555586a8b1d9c125
cindango <cindango@gmail.com>
2013-12-26 17:21:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milkshake::Application.config.secret_token = '8233ac259b5ad2d441da6800cbacc659d8a7db7be005b025bcad2230191bbb3356d76c6d8efbcf71feacc002cfcdbd87140208bff9025c696988dab9051eec03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Milkshake::Application.config.session_store :cookie_store, key: '_milkshake_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Milkshake::Application.config.session_store :active_record_store
